27cd1f2fe971403f0b3f890a87571830f3cc1b14
deepak445566 <deepakpandey1212d@gmail.com>
2026-06-09 13:32:08 +0530

    fjh

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import api from "../api/axios";
+
 
 const ProtectedRoute = ({ children }) => {
-  const token = document.cookie;
+  const [loading, setLoading] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
 
-  return token ? children : <Navigate to="/" />;
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await api.get("/auth/me");
+        setAuthenticated(true);
+      } catch (error) {
+        setAuthenticated(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (loading) return <h2>Loading...</h2>;
+
+  return authenticated ? children : <Navigate to="/" />;
 };
 
 export default ProtectedRoute;
